5f36724d681401b3b5731c637ce6e9f45d6132c4
Iris Cheung <hokyinirischeung@gmail.com>
2025-11-25 17:20:45 -0800

    added test case, revise code to pass all test case. Completed and test passed

--- main/main.py
+++ main/main.py
@@ -1,22 +1,36 @@
 def tic_tac_toe_winner(board):
+    if not check_empty_values(board) and not check_winner(board):
+        return "Tie"
+    elif check_empty_values(board) and not check_winner(board):
+        return None
+    else:
+        return check_winner(board)
 
+def check_empty_values(board):
     for row in board:
-        if row[0] == row[1] == row[2]:
-            return row[0]
-        elif board[0][0] == board[1][0] == board[2][0]:
-            return board[0][0]
-        elif board[0][1] == board[1][1] == board[2][1]:
-            return board[0][1]
-        elif board[0][2] == board[1][2] == board[2][2]:
-            return board[0][2]
-        elif board[0][0] == board[1][1] == board[2][2]:
-            return board[0][0]
-        elif board[2][0] == board[1][1] == board[0][2]:
-            return board[2][0]
-        else:
-            if row[0] == "" or row[1] == "" or row[2] == "":
-                return None
-            return "Tie"
+        for item in row:
+            if item == "":
+                return True
+
+def check_winner(board):
+    for row in board:
+        for item in row:
+            if item == "X" or item == "O":
+                if row[0] == row[1] == row[2]:
+                    return row[0]
+                elif board[0][0] == board[1][0] == board[2][0]:
+                    return board[0][0]
+                elif board[0][1] == board[1][1] == board[2][1]:
+                    return board[0][1]
+                elif board[0][2] == board[1][2] == board[2][2]:
+                    return board[0][2]
+                elif board[0][0] == board[1][1] == board[2][2]:
+                    return board[0][0]
+                elif board[2][0] == board[1][1] == board[0][2]:
+                    return board[2][0]
+
+
+
 
 
 
--- test/test_main.py
+++ test/test_main.py
@@ -74,4 +74,92 @@ def test_in_progress_case():
     # act
     result = tic_tac_toe_winner(board)
     # assert
-    assert result == None
+    assert result is None
+
+
+
+# Test from Learn PSE09
+
+def test_tic_tac_toe_winner_no_winner_returns_tie():
+    # Arrange
+    board =[
+        ['X', 'O', 'X'],
+        ['O', 'O', 'X'],
+        ['X', 'X', 'O']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result == 'Tie'
+
+def test_tic_tac_toe_winner_incomplete_no_winner_returns_none():
+    # Arrange
+    board =[
+        ['X', 'O', 'X'],
+        ['O', 'O', 'X'],
+        ['X', '', 'O']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result is None
+
+def test_tic_tac_toe_winner_empty_board_returns_none():
+    # Arrange
+    board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result is None
+
+def test_tic_tac_toe_winner_col_win_o():
+    # Arrange
+    board =[
+        ['X', 'O', 'X'],
+        ['O', 'O', 'X'],
+        ['X', 'O', 'O']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result == 'O'
+
+def test_tic_tac_toe_winner_row_win_x():
+    # Arrange
+    board =[
+        ['O', 'O', 'X'],
+        ['X', 'X', 'X'],
+        ['O', 'X', 'O']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result == 'X'
+
+def test_tic_tac_toe_winner_diag_win_o():
+    # Arrange
+    board =[
+        ['O', 'O', 'X'],
+        ['O', 'O', 'X'],
+        ['X', 'X', 'O']
+    ]
+
+    # Act
+    result = tic_tac_toe_winner(board)
+
+    # Assert
+    assert result == 'O'
